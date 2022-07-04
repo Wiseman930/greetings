f3a26fa29fe40d2aca1caeb14df785fce3c3a65b
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-07-04 18:09:05 +0200

    update

---
+++ js/greet-dom.js
@@ -0,0 +1,54 @@
+const enterNameTextBox = document.querySelector(".textName")
+const countDisplay = document.querySelector(".count")
+const greetingDisplay = document.querySelector(".greeting")
+const greetButton = document.querySelector(".greetMeButton")
+const resetCounterButton = document.querySelector(".resetCounterButton")
+
+
+let getMystoredNames = localStorage.getItem("storedNames")
+let getMystoredNames2 = JSON.parse(getMystoredNames)
+
+let getmyStoredCount = localStorage.getItem('storingMyCount')
+let getmyStoredCount2 = Number(getmyStoredCount)
+
+var displayStoredCount = 0;
+if(getmyStoredCount2){
+    displayStoredCount = getmyStoredCount2
+    countDisplay.innerHTML = displayStoredCount
+}
+const getGreet = greetMeInLangage(getMystoredNames2, displayStoredCount);
+
+greetButton.addEventListener("click", function(){
+    const NotselectLanguageButton = document.querySelector('.languageTypeRadio').value
+    getGreet.enterNameAndLanguage(enterNameTextBox.value)
+    getGreet.nocheck(NotselectLanguageButton)
+    greetingDisplay.innerHTML = getGreet.notCheckedLanguage();
+
+})
+
+greetButton.addEventListener("click", function(){
+    const selectLanguageButton = document.querySelector('.languageTypeRadio:checked').value
+    getGreet.enterNameAndLanguage(enterNameTextBox.value, selectLanguageButton)
+    greetingDisplay.innerHTML = getGreet.returnChosenLanguage();
+    countDisplay.innerHTML = getGreet.getMyCount()
+    var myCountInLocalStorage = JSON.stringify(getGreet.returnStoredInArray())
+    localStorage.setItem("storedNames", myCountInLocalStorage)
+    localStorage.setItem('storingMyCount', (getGreet.getMyCount()))
+    enterNameTextBox.value = ''
+
+})
+
+resetCounterButton.addEventListener("click", function(){
+
+    greetingDisplay.innerHTML = getGreet.returnLanguage()
+
+    var myCountInLocalStorage = JSON.stringify(getGreet.getEmptyArray())
+    localStorage.setItem("storedNames", myCountInLocalStorage)
+    localStorage.setItem('storingMyCount', (getGreet.getZeroCount()))
+    countDisplay.innerHTML = localStorage.getItem('storingMyCount')
+
+})
+
+
+
+
---
+++ test/greet.tests.js
@@ -0,0 +1,85 @@
+describe("The bill with settings factory function", function(){
+    it("should ask me to enter a name when the textbox and buttons are empty at greet click button", function(){
+        let greetings = greetMeInLangage();
+
+        greetings.enterNameAndLanguage("");
+        greetings.nocheck('emptyRdio');
+        assert.equal("Please enter your name", greetings.notCheckedLanguage());
+
+    });
+    it("should ask me to select a button when a button is not selected and the textbox is not empty ", function(){
+        let greetings = greetMeInLangage();
+
+        greetings.enterNameAndLanguage("Wiseman");
+        greetings.nocheck('emptyRdio');
+        assert.equal("Please choose a Language button", greetings.notCheckedLanguage());
+
+    });
+    it("should ask me to enter a name when the english button selected and the textbox is empty ", function(){
+        let greetings = greetMeInLangage();
+
+        greetings.enterNameAndLanguage("", "english");
+        assert.equal("Please enter your name", greetings.returnChosenLanguage());
+
+    })
+    it("should ask me to enter a name when the isiXhosa button selected and the textbox is empty ", function(){
+        let greetings = greetMeInLangage();
+
+        greetings.enterNameAndLanguage("", "isiXhosa");
+        assert.equal("Please enter your name", greetings.returnChosenLanguage());
+
+    })
+    it("should ask me to enter a name when the afrikaans button selected and the textbox is empty ", function(){
+        let greetings = greetMeInLangage();
+
+        greetings.enterNameAndLanguage("", "afrikaans");
+        assert.equal("Please enter your name", greetings.returnChosenLanguage());
+
+    })
+    it("should be able to return a name taken", function(){
+        let greetings = greetMeInLangage();
+
+        greetings.enterNameAndLanguage("Wiseman");
+        assert.equal("Wiseman", greetings.returnName());
+
+        greetings.enterNameAndLanguage("Linda");
+        assert.equal("Linda", greetings.returnName());
+    });
+    it("should be able to count the names greeted", function(){
+        let greetings = greetMeInLangage();
+
+        greetings.enterNameAndLanguage("Wiseman");
+        greetings.enterNameAndLanguage("Linda");
+        assert.equal(2, greetings.getMyCount());
+    });
+    it("should be able to count the names greeted but not twice", function(){
+        let greetings = greetMeInLangage();
+
+        greetings.enterNameAndLanguage("Wiseman");
+        greetings.enterNameAndLanguage("Wiseman");
+        greetings.enterNameAndLanguage("Linda");
+        assert.equal(2, greetings.getMyCount());
+    });
+    it("should be able to greet a person in english when a person enters their name and count", function(){
+        let greetings = greetMeInLangage();
+
+        greetings.enterNameAndLanguage('Wiseman', "english");
+        assert.equal(1, greetings.getMyCount());
+        assert.equal( 'Hello, Wiseman' , greetings.returnChosenLanguage());
+    });
+    it("should be able to greet a person in isiXhosa when a person enters their name and count", function(){
+        let greetings = greetMeInLangage();
+
+        greetings.enterNameAndLanguage('Wiseman', "isiXhosa");
+        assert.equal(1, greetings.getMyCount());
+        assert.equal( 'Molo, Wiseman' , greetings.returnChosenLanguage());
+    });
+    it("should be able to greet a person in afrikaans when a person enters their name and count", function(){
+        let greetings = greetMeInLangage();
+
+        greetings.enterNameAndLanguage('Wiseman', "afrikaans");
+        greetings.enterNameAndLanguage('Mabusela', "afrikaans");
+        assert.equal(2, greetings.getMyCount());
+        assert.equal( 'Hallo, Mabusela' , greetings.returnChosenLanguage());
+    });
+});
